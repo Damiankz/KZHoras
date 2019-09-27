737e61543614e39eea75c30bbfd66c6ce41ceb3e
Pablo <pabloml@kzsoftworks.com>
2019-09-27 12:01:19 -0300

    Add routing to calendars

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
-import Month from "./Month";
-import Calendar from "./Calendar";
+import Calendar from "./components/Calendar";
 
 function App() {
-  return <Calendar styles={{ heigth: "600px" }} />;
+  return <Calendar styles={{ heigth: "600px", fontFamily: 'Rubik' }} />;
 }
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
